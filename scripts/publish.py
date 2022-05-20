39aa24605c5bdf5cfc44e7fb1a67620f575c947b
Markus Wernersbach <wernersbacher@gmail.com>
2022-05-20 17:31:43 +0200

    added color enabling param

--- scripts/publish.py
+++ scripts/publish.py
@@ -15,6 +15,7 @@ from cv_bridge import CvBridge, CvBridgeError
 bridge = CvBridge()
         
 a = {
+    "colors": True,
     "video_stream_provider": 0,
     "fps": 25,
     "width": 320,
@@ -22,6 +23,7 @@ a = {
     "camera_name": "camera",
     "frame_id": "camera",
     "buffer_queue_size": 10
+    
 }
 args = SimpleNamespace(**a)
 
@@ -33,11 +35,13 @@ def main():
     rospy.init_node("video_publisher", anonymous=True)
 
     # load from launchfile
+    args.colors = rospy.get_param('~colors')
     args.width = rospy.get_param('~width')
     args.height = rospy.get_param('~height')
     args.fps = rospy.get_param('~fps')
     args.buffer_queue_size = rospy.get_param('~buffer_queue_size')
     args.camera_name = rospy.get_param('~camera_name')
+    args.frame_id = rospy.get_param('~frame_id', "camera")
 
     # convert to int?
     try:
@@ -47,8 +51,8 @@ def main():
 
     print ("Loaded arguments:")
     print(args)
-
-
+    
+    
     img_pub = rospy.Publisher("/" + args.camera_name + "/image_raw", Image,
                               queue_size=1)
 
@@ -76,7 +80,10 @@ def main():
         tmp, img_color = video.retrieve()
         
         # converting to gray-scale
-        img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+        if not args.colors:
+            img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+        else:
+            img = img_color
 
         if not tmp:
             print("Could not grab frame.")
@@ -100,8 +107,11 @@ def main():
 
             diff = int((img_rsz.shape[1] - args.width) / 2)
 
-            #img_out = img_rsz[:, diff:-diff-1, :]
-            img_out = img_rsz[:, diff:-diff-1]
+            if args.colors:
+                img_out = img_rsz[:, diff:-diff-1, :] 
+            else:
+                img_out = img_rsz[:, diff:-diff-1]
+
         elif aspect_ratio_in < aspect_ratio_out:
             # Output is wider than input -> crop top/bottom.
             rsz_factor = np.float(args.width) / np.float(img.shape[1])
@@ -110,8 +120,10 @@ def main():
 
             diff = int((img_rsz.shape[0] - args.height) / 2)
 
-            #img_out = img_rsz[diff:-diff-1, :, :]
-            img_out = img_rsz[diff:-diff-1, :]
+            if args.colors:
+                img_out = img_rsz[diff:-diff-1, :, :]
+            else:
+                img_out = img_rsz[diff:-diff-1, :]
         else:
             # Resize image.
             img_out = cv2.resize(img, (args.width, args.height))
@@ -121,7 +133,11 @@ def main():
 
         try:
             # Publish image.
-            img_msg = bridge.cv2_to_imgmsg(img_out, "mono8")
+            if args.colors:
+                img_msg = bridge.cv2_to_imgmsg(img_out, "bgr8")
+            else:
+                img_msg = bridge.cv2_to_imgmsg(img_out, "mono8")
+                
             img_msg.header.stamp = rospy.Time.now()
             img_msg.header.frame_id = args.frame_id
             img_pub.publish(img_msg)
